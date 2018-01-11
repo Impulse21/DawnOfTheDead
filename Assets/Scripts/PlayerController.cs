322feb56209cf655bc70ef1d291a8495264cf458
cdipaolo <christopher.dipaolo@incognito.com>
2018-01-10 21:14:09 -0500

    Added Rotate Code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
 	public float TurnRate;
 	public float Speed;
 
-	
 	// Private varables
 	private Rigidbody2D rigidBody;
 
@@ -21,22 +20,35 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		
 	}
 	
 	// Update called at a fix rate
 	void FixedUpdate()
 	{
 		processMovement();
+		processRotation();
 	}
 
-	private void processMovement()
+	protected void processMovement()
 	{
 		float horizontalMov = Input.GetAxis("Horizontal");
 		float verticalMov = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3(horizontalMov, verticalMov, 0.0f);
+		Vector2 movement = new Vector2(horizontalMov, verticalMov);
 
-		rigidBody.velocity = movement * Speed;
+		rigidBody.velocity = movement.normalized * Speed;
+	}
+
+	protected void processRotation()
+	{
+		float aimX = Input.GetAxis("AimX");
+		float aimY = Input.GetAxis("AimY");
+
+		Vector3 target = new Vector3(aimX, aimY, 0.0f);
+		Vector3 vectorToTarget = (target - transform.position).normalized;
+
+		float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 }
+
