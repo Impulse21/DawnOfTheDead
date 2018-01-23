abc2ba36a00b345307ced2d8557cdd02153149b8
cdipaolo <christopher.dipaolo@incognito.com>
2018-01-23 16:02:18 -0500

    Adding Shoot logic

---
+++ Assets/Scripts/Weapons/BaseWeaponFire.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public abstract class BaseWeaponFire : MonoBehaviour 
+{
+	public int damagePerShot = 20;                  // The damage inflicted by each bullet.
+    public float timeBetweenBullets = 0.15f;        // The time between each shot.
+    public float range = 100f;                      // The distance the gun can fire.
+	public float effectsDisplayTime	= 0.2f;			// The delay between effects
+ 	protected float timer;                          // A timer to determine when to fire.
+
+	/** Abstract methods 		*/
+	public abstract void Shoot();
+	protected abstract void DisableAffects();
+
+	/** End Abstract methods 	*/
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Add the time since Update was last called to the timer.
+        timer += Time.deltaTime;
+
+		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            // ... shoot the gun.
+             Shoot ();
+			 timer = 0;
+        }
+
+		if(timer >= timeBetweenBullets * effectsDisplayTime)
+		{
+			//DisableAffects();
+		}
+	}
+}
---
+++ Assets/Scripts/Weapons/HitScanWeaponFire.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScanWeaponFire : BaseWeaponFire 
+{
+	
+    Ray shootRay = new Ray();                       // A ray from the gun end forwards.
+    RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
+	LineRenderer gunLine;                           // Reference to the line renderer.
+    int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+
+    void Awake ()
+    {
+		gunLine = GetComponent<LineRenderer>();
+	}
+
+	public override void Shoot()
+	{
+		shootableMask = LayerMask.GetMask("Shootable");
+
+ 		shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+		// Perform the raycast against gameobjects on the shootable layer and if it hits something...
+        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        {
+            // Try and find an EnemyHealth script on the gameobject hit.
+        	//EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+
+            // Set the second position of the line renderer to the point the raycast hit.
+            gunLine.SetPosition (1, shootHit.point);
+        }
+    	else	// If the raycast didn't hit anything on the shootable layer...
+        {
+            // ... set the second position of the line renderer to the fullest extent of the gun's range.
+            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+        }
+	}
+	protected override void DisableAffects()
+	{
+		gunLine.enabled = false;
+	}
+}
