2b09112987747b0dd2fbfa9eb801cb5b181d1ccf
cdipaolo <christopher.dipaolo@incognito.com>
2018-01-23 21:17:57 -0500

    Debug Log

--- Assets/Scripts/Weapons/BaseWeaponFire.cs
+++ Assets/Scripts/Weapons/BaseWeaponFire.cs
@@ -22,6 +22,9 @@ public abstract class BaseWeaponFire : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		Vector3 fwd = transform.TransformDirection(Vector3.forward) * 100;
+		Debug.DrawRay(transform.position, fwd * 100, Color.red, 4.0f);
+
 		// Add the time since Update was last called to the timer.
         timer += Time.deltaTime;
 
@@ -36,5 +39,12 @@ public abstract class BaseWeaponFire : MonoBehaviour
 		{
 			//DisableAffects();
 		}
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, 800))
+            print("There is something in front of the object!");
 	}
 }
