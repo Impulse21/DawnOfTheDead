dca07a86afef08fd53272851c3db6b4dd7ef820f
cdipaolo <christopher.dipaolo@incognito.com>
2018-01-25 15:57:12 -0500

    Added Fireball projectile

---
+++ Assets/Scripts/Weapons/ExplodeOnImpact.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ExplodeOnImpact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public uint lifespan = 3;
+
+	 bool explode;
+
+	 void OnCollisionEnter2D(Collision2D coll) 
+	 {
+		 if(coll.gameObject.tag !=  "Player")
+		 {
+			 if(explosion == null)
+			 {
+				 return;
+			 }
+			 
+			 Explode();
+		 }
+	 }
+
+	 void Explode()
+	 {
+		 Debug.Log("Exploding");
+		 GameObject explodeObj = Instantiate(explosion, transform.position, transform.rotation);
+		 gameObject.SetActive(false);
+
+		 Destroy(explodeObj, lifespan);
+	 }
+}
