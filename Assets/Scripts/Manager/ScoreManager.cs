f995d585c082a4a0c77ca9c27dc1a4e60e6f4897
cdipaolo <christopher.dipaolo@incognito.com>
2018-02-08 15:35:54 -0500

    Added Score Manager

---
+++ Assets/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour 
+{
+	public static int score = 0;
+    public Text text;
+
+    void Awake ()
+    {
+        score = 0;
+    }
+
+
+    void Update ()
+    {
+        text.text = "Score: " + score;
+    }
+}
